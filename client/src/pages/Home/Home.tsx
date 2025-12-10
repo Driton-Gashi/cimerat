26a5ee6016ed7fd14e6e07e005b168afdf9107b8
diarxharavina <diarxharavina@gmail.com>
2025-12-10 17:14:55 +0100

    added react routes, with some placeholders/examples

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>Home page test per React Routing</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/Pagesat/Pagesat.tsx
@@ -0,0 +1,11 @@
+
+const Pagesat = () => {
+  return (
+    <div>
+        <h1>Pagesat</h1>
+        <p>Pagesat duhet te behen deri me daten xx.xx.xxxx</p>
+    </div>
+  )
+}
+
+export default Pagesat
