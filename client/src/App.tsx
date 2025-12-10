bc8281770f8dc05b3795c077e30dfba02899f7de
Driton Gashi <dritongashi1995@gmail.com>
2025-12-11 00:05:26 +0100

    deleted unnecesary files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home/Home.tsx'
-import Pagesat from './pages/Pagesat/Pagesat.tsx'
+import Home from './pages/Home/Home.tsx';
+import Pagesat from './pages/Pagesat/Pagesat.tsx';
 
 function App() {
-
    return (
       <BrowserRouter>
          <>
-         <header>
-            <nav>
-               <h1>Cimerat</h1>
-               <NavLink to='/'>Home</NavLink>
-               <NavLink to='pagesat'>Pagesat</NavLink>
-            </nav>
-         </header>
-         <Routes>
-            <Route index element={<Home />}></Route>
-            <Route path='pagesat' element={<Pagesat />}></Route>
-         </Routes>
+            <header>
+               <nav>
+                  <h1>Cimerat</h1>
+                  <NavLink to="/">Home</NavLink>
+                  <NavLink to="pagesat">Pagesat</NavLink>
+               </nav>
+            </header>
+            <Routes>
+               <Route index element={<Home />}></Route>
+               <Route path="pagesat" element={<Pagesat />}></Route>
+            </Routes>
          </>
       </BrowserRouter>
    );
