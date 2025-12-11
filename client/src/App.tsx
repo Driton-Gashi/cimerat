ee8360c530a5c0d804c6d497a0ea2f8099d0fbfc
diarxharavina <diarxharavina@gmail.com>
2025-12-11 19:54:32 +0100

    added font, and icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 import Home from './pages/Home/Home.tsx';
 import Pagesat from './pages/Pagesat/Pagesat.tsx';
+import MyIcon from './components/icons/MyIcon.tsx';
 
 function App() {
    return (
@@ -11,6 +12,9 @@ function App() {
                   <h1>Cimerat</h1>
                   <NavLink to="/">Home</NavLink>
                   <NavLink to="pagesat">Pagesat</NavLink>
+                  <NavLink to="#">
+                     <MyIcon iconName="dashboard" />
+                  </NavLink>
                </nav>
             </header>
             <Routes>
